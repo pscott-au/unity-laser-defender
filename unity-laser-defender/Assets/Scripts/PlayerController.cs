2905aac48fc342690ca0c7bbc8406002fe212b3f
Peter Scott <peter@shotgundriver.com>
2017-07-19 13:43:53 +1000

    Shooting Enemies
    Udemy -> 112

---
+++ unity-laser-defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float playerSpeed  = 5.0f;
+	float xmin;
+	float xmax;
+	public float padding = 1;
+
+	public GameObject projectile;
+	public float projectileSpeed;
+	public float firingRate = 0.2f;
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint( new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint( new Vector3(1,0,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+
+	void Fire() {
+		GameObject beam =Instantiate(projectile, transform.position, Quaternion.identity ) as GameObject;
+		// beam.rigidbody2D.velocity = new Vector3 (0, projectileSpeed,0);
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3 (0, projectileSpeed,0);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			// transform.position += new Vector3 (-playerSpeed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+		} 
+		else if (Input.GetKey(KeyCode.RightArrow)  )
+		{
+			//transform.position += new Vector3(playerSpeed * Time.deltaTime, 0,0);
+			transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+		} 
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.00001f, firingRate);
+		}
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			//InvokeRepeating ("Fire", 0.00001f, firingRate);
+			CancelInvoke("Fire");
+		}
+
+
+		// restrict player to gamespace.
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector3 ( newX, transform.position.y, transform.position.z );
+	}
+}
---
+++ unity-laser-defender/Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+
+	public float damage = 10f;
+
+
+	public float GetDamage() {
+		return damage;
+	}
+	// Use this for initialization
+	public void Hit() {
+		Debug.Log ("HIT TRIGGERED");
+		Destroy (gameObject);
+	}
+}
